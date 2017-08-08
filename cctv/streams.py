ec5f518daa9a12b31f8d1904e58b6f3fecda5ce6
antikantian <lesterm@mac.com>
2017-08-08 21:47:35 +0300

    ...

--- cctv/streams.py
+++ cctv/streams.py
@@ -4,24 +4,24 @@ from omxplayer import OMXPlayer
 from omxplayer.keys import *
 
 streams = [
-    "rtsp://admin:12345678@192.168.10.240:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.241:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.242:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.243:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.244:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.245:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.246:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.247:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.248:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://admin:12345678@192.168.10.249:554/cam/realmonitor?channel=1&subtype=1",
-    "rtsp://192.168.10.170:554/stander/livestream/0/1",
-    "rtsp://192.168.10.171:554/stander/livestream/0/1"
+    "rtsp://admin:12345678@192.168.10.240:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.241:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.242:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.243:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.244:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.245:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.246:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.247:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.248:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://admin:12345678@192.168.10.249:554/cam/realmonitor?channel=1&subtype=0",
+    "rtsp://192.168.10.170:554/stander/livestream/0/0",
+    "rtsp://192.168.10.171:554/stander/livestream/0/0"
 ]
 
 app = Flask(__name__)
 api = Api(app)
 
-player = OMXPlayer(streams[0], args=['--no-osd', '--live', '--blank'])
+player = OMXPlayer(streams[0], args=['--no-osd', '--live', '--blank', '--threshold=0'])
 
 
 class CameraStream(Resource):

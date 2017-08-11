c44f383640b4d0adbba5c5bfc2878cff4616dd6d
antikantian <lesterm@mac.com>
2017-08-11 12:11:17 +0300

    ...

--- cctv/streams.py
+++ cctv/streams.py
@@ -77,21 +77,7 @@ class TiledPlayers(object):
             ]
         )
 
-    def hide_all(self):
-        self.p1.action(HIDE_VIDEO)
-        self.p2.action(HIDE_VIDEO)
-        self.p3.action(HIDE_VIDEO)
-        self.p4.action(HIDE_VIDEO)
-        self.p5.action(HIDE_VIDEO)
-        return True
 
-    def unhide_all(self):
-        self.p1.action(UNHIDE_VIDEO)
-        self.p2.action(UNHIDE_VIDEO)
-        self.p3.action(UNHIDE_VIDEO)
-        self.p4.action(UNHIDE_VIDEO)
-        self.p5.action(UNHIDE_VIDEO)
-        return True
 
 
 app = Flask(__name__)
@@ -108,8 +94,80 @@ player = OMXPlayer(
     ]
 )
 
+c1 = "rtsp://admin:12345678@192.168.10.240:554/cam/realmonitor?channel=1&subtype=1"
+c2 = "rtsp://admin:12345678@192.168.10.241:554/cam/realmonitor?channel=1&subtype=1"
+c3 = "rtsp://admin:12345678@192.168.10.242:554/cam/realmonitor?channel=1&subtype=1"
+c4 = "rtsp://admin:12345678@192.168.10.243:554/cam/realmonitor?channel=1&subtype=1"
+c5 = "rtsp://admin:12345678@192.168.10.244:554/cam/realmonitor?channel=1&subtype=1"
+p1 = OMXPlayer(
+    c1,
+    args=[
+        '--avdict="rtsp_transport:tcp"',
+        '--threshold=.01',
+        '--video_fifo=.01',
+        '--win=16,0,368,288'
+    ]
+)
+p2 = OMXPlayer(
+    c2,
+    args=[
+        '--avdict="rtsp_transport:tcp"',
+        '--threshold=.01',
+        '--video_fifo=.01',
+        '--layer=1',
+        '--win=400,0,752,288'
+    ]
+)
+p3 = OMXPlayer(
+    c3,
+    args=[
+        '--avdict="rtsp_transport:tcp"',
+        '--threshold=.01',
+        '--video_fifo=.01',
+        '--layer=1',
+        '--win=784,0,1136,288'
+    ]
+)
+p4 = OMXPlayer(
+    c4,
+    args=[
+        '--avdict="rtsp_transport:tcp"',
+        '--threshold=.01',
+        '--video_fifo=.01',
+        '--layer=1',
+        '--win=1168,0,1520,288'
+    ]
+)
+p5 = OMXPlayer(
+    c5,
+    args=[
+        '--avdict="rtsp_transport:tcp"',
+        '--threshold=.01',
+        '--video_fifo=.01',
+        '--layer=1',
+        '--win=1552,0,1904,288'
+    ]
+)
+
+
+def hide_all():
+    p1.action(HIDE_VIDEO)
+    p2.action(HIDE_VIDEO)
+    p3.action(HIDE_VIDEO)
+    p4.action(HIDE_VIDEO)
+    p5.action(HIDE_VIDEO)
+    return True
+
+
+def unhide_all():
+    p1.action(UNHIDE_VIDEO)
+    p2.action(UNHIDE_VIDEO)
+    p3.action(UNHIDE_VIDEO)
+    p4.action(UNHIDE_VIDEO)
+    p5.action(UNHIDE_VIDEO)
+    return True
+
 current_cam = 0
-tiles = TiledPlayers()
 
 
 class CameraStream(Resource):
@@ -125,11 +183,11 @@ class CameraTile(Resource):
     def get(self, action):
         if action == 'tile':
             player.action(HIDE_VIDEO)
-            tiles.unhide_all()
+            unhide_all()
             return {'stream': 'tiled'}
         else:
             player.action(UNHIDE_VIDEO)
-            tiles.hide_all()
+            hide_all()
             return {'stream': 'not_tiled'}
 
 

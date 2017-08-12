8a7ca688c97e1649994147ce36f35b3fc9e19471
antikantian <lesterm@mac.com>
2017-08-12 12:08:52 +0300

    ...

--- cctv/streams.py
+++ cctv/streams.py
@@ -36,7 +36,8 @@ for i in range(len(streams)):
                 "--threshold=0",
                 "--video_fifo=0",
                 "--timeout=0",
-                "--genlog"
+                "--genlog",
+                "--stats"
             ]
         )
     else:
